f9f0f6718e261cec209b94a4f392062c539e792f
reidtc82 <reidtc82@gmail.com>
2021-08-16 22:19:46 -0700

    stuff

--- src/start-kafka.sh
+++ src/start-kafka.sh
@@ -1,3 +1,4 @@
+python 
 zKServer start
 kafka-server-start /usr/local/etc/kafka/server.properties
 kafka-topics --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic test-crypto
