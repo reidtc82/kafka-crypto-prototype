f5a5e99f2ded42948847c68d2a5ca036ea989a91
reidtc82 <reidtc82@gmail.com>
2021-08-16 22:21:01 -0700

    finishing up

--- src/start-kafka.sh
+++ src/start-kafka.sh
@@ -1,4 +1,5 @@
-python 
+source crypto/bin/activate
+pip install -r requirements.txt
 zKServer start
 kafka-server-start /usr/local/etc/kafka/server.properties
 kafka-topics --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic test-crypto
