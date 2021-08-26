0dc5d3130a53fbcf2ac350c52f54306c8826363e
reidtc82 <reidtc82@gmail.com>
2021-08-26 13:10:40 -0700

    building bot and adding ohlc

--- src/producer.py
+++ src/producer.py
@@ -1,7 +1,6 @@
 from kafka import KafkaProducer
 import time
 import cryptowatch as cw
-import time
 from google.protobuf.json_format import MessageToJson
 import json
 import uuid
@@ -9,7 +8,7 @@ import uuid
 producer = KafkaProducer(bootstrap_servers="localhost:9092", api_version=(0, 10))
 
 
-def run_sample(subs, sleep_period):
+def run_trade_updates(subs, sleep_period):
     # Subscribe to resources (https://docs.cryptowat.ch/websocket-api/data-subscriptions#resources)
     cw.stream.subscriptions = subs
 
@@ -23,6 +22,19 @@ def run_sample(subs, sleep_period):
 
     # for i in range(100):
     #     time.sleep(sleep_period)
+
+
+def run_ohlc_updates(subs, sleep_period):
+    # Subscribe to resources (https://docs.cryptowat.ch/websocket-api/data-subscriptions#resources)
+    cw.stream.subscriptions = subs
+
+    cw.stream.on_intervals_update = handle_intervals_update
+    # Start receiving
+    cw.stream.connect()
+
+    time.sleep(sleep_period)
+
+    cw.stream.disconnect()
 
 
 # What to do on each trade update
@@ -55,5 +67,34 @@ def handle_trades_update(trade_update):
         producer.send("crypto-stream", bytes(msg, encoding="utf8"))
 
 
+def handle_intervals_update(interval_update):
+    market_msg = ">>> Market#{} Exchange#{} Pair#{}".format(
+        interval_update.marketUpdate.market.marketId,
+        interval_update.marketUpdate.market.exchangeId,
+        interval_update.marketUpdate.market.currencyPairId,
+    )
+    #     print(market_msg)
+
+    temp = []
+    for interval in interval_update.marketUpdate.intervalsUpdate.intervals:
+        interval_msg = (
+            "\tTIMESTAMP:{} OHLC:{}, {}, {}, {} VOLUMEBASE: {} VOLUMEQUOTE: {}".format(
+                interval.closetime,
+                interval.ohlc.openStr,
+                interval.ohlc.highStr,
+                interval.ohlc.lowStr,
+                interval.ohlc.closeStr,
+                interval.volumeBaseStr,
+                interval.volumeQuoteStr,
+            )
+        )
+
+        item = json.loads(MessageToJson(interval))
+        item["id"] = str(uuid.uuid4())
+        msg = json.dumps(item)
+        producer.send("crypto-stream", bytes(msg, encoding="utf8"))
+
+
 if __name__ == "__main__":
-    run_sample(["markets:*:trades"], 100)
+    run_trade_updates(["markets:*:trades"], 1)
+    run_ohlc_updates(["assets:60:ohlc"], 1)
